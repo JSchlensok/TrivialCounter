dc6093941716063d441afc28faf10965f0b16486
JSchlensok <34397338+JSchlensok@users.noreply.github.com>
2019-10-08 14:26:16 +0200

    Update schema.sql
    Added some columns

--- schema.sql
+++ schema.sql
@@ -1,13 +1,19 @@
 CREATE TABLE lecturers (
 	id int AUTO_INCREMENT PRIMARY KEY,
 	name varchar(255) NOT NULL,
-	added datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	added datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
 )
 
+CREATE TABLE semester (
+	id int AUTO_INCREMENT PRIMARY KEY,
+	name varchar(255) NOT NULL
+)
 CREATE TABLE classes (
 	id int AUTO_INCREMENT PRIMARY KEY,
 	name varchar(255) NOT NULL,
 	added datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	semester_id NOT NULL,
+	FOREIGN KEY (semester_id) REFERENCES (semester)
 )
 
 CREATE TABLE lecture_trivial_count (
@@ -16,7 +22,8 @@ CREATE TABLE lecture_trivial_count (
 	lecture_time datetime NOT NULL,
 	lecturer_id int NOT NULL,
 	class_id int NOT NULL,
-	count int NOT NULL,
+	trivial_count int NOT NULL,
+	duration time NOT NULL,
 	FOREIGN KEY (lecturer_id) REFERENCES (lecturers),
 	FOREIGN KEY (class_id) REFERENCES (classes)
 )
