9463ff47c8f943d32de4bec49af0f0c6b2defac8
JSchlensok <34397338+JSchlensok@users.noreply.github.com>
2019-10-21 11:06:43 +0200

    Beautification

--- python/getStreamLinks.py
+++ python/getStreamLinks.py
@@ -7,6 +7,7 @@ response = requests.get(url)
 page = str(BeautifulSoup(response.content))
 all_urls = []
 
+# Download all URLs from the TUM livestream page
 def getURL(page):
 	# https://stackoverflow.com/questions/15517483/how-to-extract-urls-from-an-html-page-in-python
     start_link = page.find("a href")
@@ -17,7 +18,6 @@ def getURL(page):
     url = page[start_quote + 1: end_quote]
     return url, end_quote
 
-# Collect URLs to Streams
 while True:
     url, n = getURL(page)
     page = page[n:]
@@ -27,17 +27,14 @@ while True:
         break
 
 
-urls = [url for url in all_urls if bool(re.search(r"COMB$", url))] # Take only the URLs matching the regex
-
-print(urls)
+urls = [url for url in all_urls if bool(re.search(r"COMB$", url))] # Keep only stream URLs
 
 stream_urls = []
 
+# Get the m3u8 link for every stream
 for url in urls:
 	response = requests.get(f"https://live.rbg.tum.de{url}")
 	# page = str(BeautifulSoup(response.content))
 	print(response.text)
 	stream_url = re.search("https://.*\.m3u8", response.text)
 	stream_urls.append(stream_url.group(0))
-
-print(stream_urls)
